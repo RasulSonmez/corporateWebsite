fbf9934fdf5141d4e75cfed7e91b2dd9592d9ef2
rasul sönmez <barbaros743@outlook.com>
2021-11-01 22:39:08 +0300

    5ththth

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,99 +1,87 @@
 //Show Menu
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId);
-    nav = document.getElementById(navId);
-    if(toggle && nav){
-        toggle.addEventListener('click', () =>{
-            nav.classList.toggle('show-menu')
-        })
-    }
-}
-showMenu('nav-toggle', 'nav-menu');
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  nav = document.getElementById(navId);
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show-menu");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 //Remove Menu Mobile
-const navLink = document.querySelectorAll('.nav__link');
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu');
-    navMenu.classList.remove('show-menu');
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
-
-//Scrol Sections Avtive Link
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 50,
-              sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 //Change Background Header
-function scrollHeader(){
-    const nav = document.getElementById('header')
-    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+function scrollHeader() {
+  const nav = document.getElementById("header");
+  // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 80) nav.classList.add("scroll-header");
+  else nav.classList.remove("scroll-header");
 }
-window.addEventListener('scroll', scrollHeader)
+window.addEventListener("scroll", scrollHeader);
 
 //Dark Light Theme
-const themeButton = document.getElementById('theme-button')
-const darkTheme = 'dark-theme'
-const iconTheme = 'bx-toggle-right'
+const themeButton = document.getElementById("theme-button");
+const darkTheme = "dark-theme";
+const iconTheme = "bx-toggle-right";
 
 // Previously selected topic (if user selected)
-const selectedTheme = localStorage.getItem('selected-theme')
-const selectedIcon = localStorage.getItem('selected-icon')
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
 
 // We obtain the current theme that the interface has by validating the dark-theme class
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-toggle-left' : 'bx-toggle-right'
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme)
+    ? "bx-toggle-left"
+    : "bx-toggle-right";
 
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
-  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
-  themeButton.classList[selectedIcon === 'bx-toggle-left' ? 'add' : 'remove'](iconTheme)
+  document.body.classList[selectedTheme === "dark" ? "add" : "remove"](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === "bx-toggle-left" ? "add" : "remove"](
+    iconTheme
+  );
 }
 
 // Activate / deactivate the theme manually with the button
-themeButton.addEventListener('click', () => {
-    // Add or remove the dark / icon theme
-    document.body.classList.toggle(darkTheme)
-    themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
-    localStorage.setItem('selected-theme', getCurrentTheme())
-    localStorage.setItem('selected-icon', getCurrentIcon())
-})
-
+themeButton.addEventListener("click", () => {
+  // Add or remove the dark / icon theme
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  // We save the theme and the current icon that the user chose
+  localStorage.setItem("selected-theme", getCurrentTheme());
+  localStorage.setItem("selected-icon", getCurrentIcon());
+});
 
 ///SCROLL REVEAL
 ScrollReveal().reveal(".ani-left", {
-    origin: "left",
-    distance: "10rem",
-    duration: 1000,
-    easing: "ease-in",
-  });
-  
-  ScrollReveal().reveal(".ani-right", {
-    origin: "right",
-    distance: "10rem",
-    duration: 1000,
-    easing: "ease-in",
-  });
-  
-  ScrollReveal().reveal(".ani-fade-up", {
-    opacity: 0,
-    easing: "ease-in",
-    duration: 1500,
-  });
+  origin: "left",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-right", {
+  origin: "right",
+  distance: "10rem",
+  duration: 1000,
+  easing: "ease-in",
+});
+
+ScrollReveal().reveal(".ani-fade-up", {
+  opacity: 0,
+  easing: "ease-in",
+  duration: 1500,
+});
