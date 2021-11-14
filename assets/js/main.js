f4b84106973a05cac4d25e4413f3ccd277d18c89
ahmet yıkar <ahmetyikar@hotmail.com.tr>
2021-11-14 18:13:46 +0300

    dd

--- assets/js/main.js
+++ assets/js/main.js
@@ -85,3 +85,52 @@ ScrollReveal().reveal(".ani-fade-up", {
   easing: "ease-in",
   duration: 1500,
 });
+
+
+function setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires="+ d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i <ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function eraseCookie(name) {   
+  document.cookie = name+'=; Max-Age=-99999999;';  
+}
+
+function getPageOfCulture(){
+  var selectedCulture=getCookie("culture");
+
+  if(selectedCulture==null){  
+        var userLang = navigator.language || navigator.userLanguage; 
+        selectedCulture=userLang;
+  }
+ 
+  if(selectedCulture==="tr"){
+    $(".tr").show();
+    $(".en").hide();
+    $("#option-tr").attr("selected",true);
+    $("#option-en").attr("selected",false);
+  }
+  else{
+    $(".tr").hide();
+    $(".en").show();
+    $("#option-tr").attr("selected",false);
+    $("#option-en").attr("selected",true);
+  }
+}
